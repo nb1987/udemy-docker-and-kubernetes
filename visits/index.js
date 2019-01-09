05050974ce8c88793e899e212fb5a96797c3e212
nb1987 <nbagnall@gmail.com>
2019-01-08 20:43:59 -0600

    Add new visits sub-project

---
+++ visits/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
+
+const app = express();
+const redisClient = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+redisClient.set('visits', 0);
+
+app.get('/', (req, res) => {
+    redisClient.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        redisClient.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Listening on port 8081');
+});
